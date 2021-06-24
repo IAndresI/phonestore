2c2b4b891385946fea379edfcef455d2492f178f
IAndresI <andrei200277@gmail.com>
2021-06-25 00:59:32 +0300

    add routes

--- server/controllers/phoneController.js
+++ server/controllers/phoneController.js
@@ -9,16 +9,25 @@ class PhoneController {
     if(!id) {
       return next(ApiError.badRequest('Enter ID!'))
     }
-    const qeury = await db.query('SELECT * FROM phone WHERE phone_id=$1', [id]);
+    const qeury = await db.query('SELECT ph.*,manuf.name as "manufacturer" FROM phone ph INNER JOIN manufacturer manuf ON manuf.manufacturer_id=ph.manufacturer_id WHERE phone_id=$1', [id]);
     const data = qeury.rows[0]
     return res.json(data)
   }
 
   async getAll(req, res,next) {
     try {
-      const qeury = await db.query('SELECT * FROM phone');
-      const data = qeury.rows
-      return res.json(data)
+      const {page, limit, sort, color, manufacturers, price} = req.query;
+      const offset = page*limit-limit;
+      const orderBy = `${sort ? `ORDER BY ph.price ${sort}` : ""}`;
+      const whereColor = color ? `AND col_det.color_id IN(${color.join(',')})` : "";
+      const whereManufacturer = manufacturers ? `AND ph.manufacturer_id IN(${manufacturers.join(',')})` : "";
+      const wherePrice = price ? `AND ph.price > ${price[0]}::money AND ph.price < ${price[1]}::money` : "";
+
+      //console.log(`SELECT ph.* FROM phone ph INNER JOIN color_details col_det ON col_det.phone_id=ph.phone_id WHERE TRUE ${whereColor} ${whereManufacturer} ${wherePrice} GROUP BY ph.phone_id ${orderBy} LIMIT ${limit} OFFSET ${offset};`);
+
+      const qeury = await db.query(`SELECT ph.* FROM phone ph INNER JOIN color_details col_det ON col_det.phone_id=ph.phone_id WHERE TRUE ${whereColor} ${whereManufacturer} ${wherePrice} GROUP BY ph.phone_id ${orderBy} LIMIT ${limit} OFFSET ${offset};`);
+      const countQeury = await db.query(`SELECT COUNT(*) FROM phone ph INNER JOIN color_details col_det ON col_det.phone_id=ph.phone_id WHERE TRUE ${whereColor} ${whereManufacturer} ${wherePrice} GROUP BY ph.phone_id;`);
+      return res.json({count: countQeury.rows.length,phones: qeury.rows})
     }
     catch(err) {
       return next(ApiError.badRequest(err.message));
@@ -30,6 +39,17 @@ class PhoneController {
     try {
       const qeury = await db.query('select * from get_min_max_price();');
       const [data] = qeury.rows
+      return res.json(data);
+    }
+    catch(err) {
+      return next(ApiError.badRequest(err.message));
+    }
+  }
+
+  async getAllColors(req, res,next) {
+    try {
+      const qeury = await db.query('select * from color;');
+      const data = qeury.rows
       return res.json(data);
     }
     catch(err) {
--- server/routes/phoneRouter.js
+++ server/routes/phoneRouter.js
@@ -3,6 +3,7 @@ const router = new Router();
 const phoneController = require('../controllers/phoneController');
 
 router.get('/min_max_price', phoneController.getMinMaxPirce);
+router.get('/color',phoneController.getAllColors);
 router.post('/',phoneController.create);
 router.get('/',phoneController.getAll);
 router.get('/:id',phoneController.getOne);
